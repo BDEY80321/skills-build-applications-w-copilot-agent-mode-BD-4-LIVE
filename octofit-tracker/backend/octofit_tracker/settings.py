ca19a2d2751d075eb0c4b12703acadb685abc7a9
BDEY80321 <bibhas.dey@rapidcircle.com>
2025-04-08 13:21:03 +0000

    Step4 Done

--- octofit-tracker/backend/octofit_tracker/settings.py
+++ octofit-tracker/backend/octofit_tracker/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-#_zok9&06izt^73zt@5j+s_3rj0$i%t-6ov+0-t8#vn)kh0i*l
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '[REPLACE-THIS-WITH-YOUR-CODESPACE-NAME]-8000.app.github.dev']
 
 
 # Application definition
